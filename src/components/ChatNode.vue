dccbc208e0629c95fee05ef4b4a864afd9b0d2fb
Josh Perry <joshperry@outlook.com>
2021-07-11 23:07:21 +0100

    Deleted hello world, added stub for node and tree

---
+++ src/components/ChatNode.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>node</p>
+
+    <ChatNode v-for="child in children" v-bind:key="child.id" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatNode',
+  data() {
+    return {
+      children: []
+    }
+  },
+  async created() {
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/TreeChat.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>tree</h1>
+    <ChatNode />
+  </div>
+</template>
+
+<script>
+import ChatNode from './ChatNode'
+
+export default {
+  name: 'TreeChat',
+  components: {
+    ChatNode
+  }
+}
+</script>
+
+<style scoped>
+</style>
